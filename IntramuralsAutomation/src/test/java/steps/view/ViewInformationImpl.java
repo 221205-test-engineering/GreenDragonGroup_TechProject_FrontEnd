a317122d393da0a5a69be5550d847eb7845e595e
Amy Chan <amy.chansya@gmail.com>
2023-01-11 11:10:00 -0500

    Add subpackages to steps package

---
+++ IntramuralsAutomation/src/test/java/steps/view/ViewInformationImpl.java
@@ -0,0 +1,12 @@
+package steps.view;
+import pages.*;
+
+//import target runner class
+
+import org.openqa.selenium.*;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ViewInformationImpl {
+}
